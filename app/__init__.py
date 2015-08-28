b72f1e8fa9f4b3a48b12db7000531516a7d67bd5
Adi Foulger <afoulger@fandango.com>
2015-08-28 16:45:45 -0700

    Implment better crypto workflow per Wayne

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,14 @@ from urllib import quote
 from flask import Flask, render_template, request, Response
 from redis import Redis
 
+from Crypto import Random
 from Crypto.Cipher import AES
-from Cypto.Hash import HMAC
+from Crypto.Hash.HMAC import HMAC
 
-URL_BASE = 'http://localhost:5000/'
+URL_BASE = 'https://tagging.fandangousa.com:5000?-='
 
-_AES = AES.new(urandom(32), AES.MODE_CBC)
+_IV = Random.new().read(AES.block_size)
+_KEY = urandom(32)
 HASH_KEY = urandom(32)
 app = Flask(__name__)
 
@@ -21,35 +23,41 @@ vals = {}
 
 @app.route('/')
 def index():
+    url_arg = request.args.get('-')
+    if url_arg is not None:
+        _id, _key, _key_hash = b64decode(url_arg).split('||')
+        if _key_hash == HMAC(HASH_KEY, _key).digest():
+            _aes = AES.new(_KEY, AES.MODE_CBC, _IV)
+            key = _aes.decrypt(_key)
+            encrypted_msg = vals.get(_id)
+            if encrypted_msg is not None:
+                vals.pop(_id)
+                aes = AES.new(key, AES.MODE_CBC, _IV)
+                msg = aes.decrypt(b64decode(encrypted_msg))
+                return render_template('getMessage.html', message=msg.strip())
+            else:
+                return render_template('noMessage.html'), 404
+    
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
     ##Create a random password
     key = urandom(32)
-    aes = AES.new(key, AES.MODE_CBC)
-    _key = _AES.encrypt(key)
+    iv = Random.new().read(AES.block_size)
+    aes = AES.new(key, AES.MODE_CBC, _IV)
+    _aes = AES.new(_KEY, AES.MODE_CBC, _IV)
+    _key = _aes.encrypt(key)
 
     message = request.form.get('message', '')
-    message += ' '*(16-(len(message) % 16 or 16))
+    message += ' '*(AES.block_size-(len(message) % AES.block_size or AES.block_size))
 
     _id = uuid4().hex
     vals[_id] = b64encode(aes.encrypt(message))
 
     return render_template('getMessage.html', message=URL_BASE + quote(b64encode('%s||%s||%s'%(_id, _key, HMAC(HASH_KEY, _key).digest())), safe=''))
 
-@app.route('/<string:_id>')
-def retreive(_id):
-    msg_encrypted = vals.get(_id)
-
-    
-    if msg_encrypted is None or key is None:
-        return render_template('noMessage.html'), 404
-    
-    vals.pop(_id)
-    aes = AES.new(b64decode(key))
-    return render_template('getMessage.html', message=aes.decrypt(b64decode(msg_encrypted)).strip())
 
 
 if __name__ == '__main__':
-    app.run(port=666)
+    app.run(debug=True)
